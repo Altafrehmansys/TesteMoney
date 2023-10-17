dd68fb8dd694a28803275511e5eb57759386d6d6
Altaf Ur Rehman <altaf.rehman@iqeqdigital.com>
2023-10-17 12:03:11 +0500

    Commit sdk

---
+++ TesteMoney/TesteMoney/TesteMoney.h
@@ -0,0 +1,18 @@
+//
+//  TesteMoney.h
+//  TesteMoney
+//
+//  Created by Altaf Ur Rehman on 17/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TesteMoney.
+FOUNDATION_EXPORT double TesteMoneyVersionNumber;
+    
+//! Project version string for TesteMoney.
+FOUNDATION_EXPORT const unsigned char TesteMoneyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TesteMoney/PublicHeader.h>
+
+
